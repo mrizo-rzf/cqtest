350073cca16e7ea1c57d06a3f52f05b7c5f43116
Mauricio Rizo <mauricio.rizo@boszdigital.com>
2014-03-08 22:40:38 -0600

    Initial commit

---
+++ training/components-workshop/components-workshop-taglib/src/main/java/com/prodigious/workshop/components/taglib/osgi/Activator.java
@@ -0,0 +1,19 @@
+package com.prodigious.workshop.components.taglib.osgi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squeakysand.osgi.framework.BasicBundleActivator;
+
+/**
+ * Bundle activator for com.prodigious.workshop.components - components-workshop-taglib.
+ */
+public class Activator extends BasicBundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public Activator() {
+		super(LOG);
+	}
+
+}
---
+++ training/components-workshop/components-workshop-view/src/main/content/jcr_root/apps/components-workshop/components/examplecomponent/examplecomponent.jsp
@@ -0,0 +1,13 @@
+<%--
+
+    This is a simple non-page component using JSP that extends the foundation parbase component.
+
+--%>
+
+<%@ include file="/apps/components-workshop/components/global.jspx" %>
+
+<%@ taglib prefix="cqcw" uri="http://components.workshop.prodigious.com/taglibs/components-workshop-taglib" %>
+
+<p>This is an example non-page component (examplecomponent).</p>
+
+<h1><cqcw:helloWorld name="CQ Blueprints User"/></h1>
